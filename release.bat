684007e9c81ccd925ca9dac37eed17fe47953130
mrexodia <mr.exodia.tpodt@gmail.com>
2016-07-23 21:48:01 +0200

    changed directory structure

--- release.bat
+++ release.bat
@@ -1,6 +1,6 @@
 @echo off
 rmdir /S /Q release
-xcopy x64dbgpy_plugins release\x32\plugins\x64dbgpy_plugins\* /S /Y
-xcopy x64dbgpy_plugins release\x64\plugins\x64dbgpy_plugins\* /S /Y
+xcopy x64dbgpy_plugins release\x32\plugins\* /S /Y
+xcopy x64dbgpy_plugins release\x64\plugins\* /S /Y
 copy bin\x32\*.dp32 release\x32\plugins\
 copy bin\x64\*.dp64 release\x64\plugins\
